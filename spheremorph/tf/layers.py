9387a680bf9a311d92192e3b6753797a70f881d8
Jian Li (Andrew) <jian.li.andrew@gmail.com>
2024-04-01 11:39:20 -0400

    added Spherical2D and NJF layers

--- spheremorph/tf/layers.py
+++ spheremorph/tf/layers.py
@@ -9,7 +9,7 @@ import numpy as np
 import tensorflow as tf
 from keras.layers import Layer
 
-from .utils import unpad_2d_image, pad_2d_image_spherically
+from .utils import unpad_2d_image, pad_2d_image_spherically, jacobian_2d, gaussian_filter_2d
 
 
 class LossEndPoint(Layer):
@@ -168,6 +168,88 @@ class ConcatWithPositionalEncoding(KL.Layer):
 
     def compute_output_shape(self, input_shape, **kwargs):
         return input_shape[0:-1] + (input_shape[-1] + 2 * self.npos,)
+
+
+class SphericalConv2D(KL.Conv2D):
+    """
+    Conv2D layer with spherical unpadding and padding
+    """
+
+    def __init__(self, filters, kernel_size, pad_size=0, is_flow=False, **kwargs):
+        super(SphericalConv2D, self).__init__(filters, kernel_size, **kwargs)
+        self.pad_size = pad_size
+        self.is_flow = is_flow
+
+    def call(self, x):
+        y = super().call(x)
+        if self.pad_size > 0:
+            y = unpad_2d_image(y, self.pad_size)
+            y = pad_2d_image_spherically(y, self.pad_size, is_flow=self.is_flow)
+            y.set_shape(self.compute_output_shape(x.shape))
+        return y
+
+
+class NegativeJacobianFiltering2D(Layer):
+    """
+    Keras Layer: gaussian filtering only at locations with negative jacobian
+    this layer does remove negative jacobian but introduce unnatural flows
+    need further test
+    """
+
+    def __init__(self, filter_shape=(2, 2), sigma=0.7, pad_size=0, max_iter=3, **kwargs):
+        super(NegativeJacobianFiltering2D, self).__init__(**kwargs)
+        self.filter_shape = filter_shape
+        self.sigma = sigma
+        self.pad_size = pad_size
+        self.max_iter = max_iter
+
+    def build(self, input_shape):
+        if input_shape[-1] != 2:
+            raise ValueError(f'The last dimension should be 2, but got {input_shape[-1]}')
+        # cache the negative jacobian locations so it will shared by cond and body
+        H, W = input_shape[1:3]
+        tf_zeros = tf.zeros((1, H, W, 1), dtype=tf.bool)
+        self.J_neg = tf.Variable(tf_zeros, shape=(None, H, W, 1), trainable=False, dtype=tf.bool)
+        super(NegativeJacobianFiltering2D, self).build(input_shape)
+
+    def call(self, x):
+        def _dilate_mask(z, f_size):
+            filter = tf.ones((*f_size, 1), dtype=z.dtype)
+            return tf.nn.dilation2d(z, filter, strides=[1, 1, 1, 1],
+                                    padding='SAME', data_format='NHWC', dilations=[1, 1, 1, 1])
+
+        def _update_J_neg(z):
+            # compute jacobian determinant
+            J = tf.map_fn(lambda w: jacobian_2d(w, det=True, is_replace_nan=True), z)
+            # compute negative jac and expand the last dim because jacobian_2d reduced it
+            J_neg = tf.expand_dims(tf.less(J, 0), -1)
+            # dilate the mask according to the range of gaussian filter
+            # f_size = [int(w * 1.5 // 2 * 2 + 1) for w in self.filter_shape]
+            f_size = self.filter_shape
+            self.J_neg = tf.cast(_dilate_mask(tf.cast(J_neg, tf.int32), f_size), tf.bool)
+
+        def cond(z):
+            num_neg = tf.reduce_sum(tf.cast(self.J_neg, tf.int32))
+            return tf.greater(num_neg, 0)
+
+        def body(z):
+            # replace the negative jacobian locations (and their neighbors) with gaussian filtered values
+            y = tf.where(self.J_neg, gaussian_filter_2d(z, self.filter_shape, self.sigma), z)
+            if self.pad_size > 0:
+                y = unpad_2d_image(y, self.pad_size)
+                y = pad_2d_image_spherically(y, self.pad_size, is_flow=True)
+                y.set_shape(z.shape)
+
+            _update_J_neg(y)
+            return (y,)
+
+        # pre-update J_neg once at the beginning
+        _update_J_neg(x)
+
+        return tf.while_loop(cond, body, [x], maximum_iterations=self.max_iter)[0]
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
 
 
 def Stack(axis=-1, **kwargs):
